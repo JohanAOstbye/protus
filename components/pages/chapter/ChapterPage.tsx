b051d95f26d7171e1b4fe5478ae486e84d0d5237
Vemund Eggemoen <vemundeggemoen@Vemunds-MacBook-Pro.local>
2023-02-01 17:07:26 +1100

    created layout for mainpage

--- components/pages/chapter/ChapterPage.tsx
+++ components/pages/chapter/ChapterPage.tsx
@@ -1,4 +1,9 @@
 import AlertBanner from 'components/elements/AlertBanner'
+import BlogContainer from 'components/blocks/BlogContainer'
+import Sidebar from 'components/layout/Sidebar'
+import BlogHeader from 'old/BlogHeader'
+import Header from 'components/layout/Header'
+import Footer from 'components/layout/Footer'
 
 export default function ChapterLayout({
   preview = false,
@@ -14,6 +19,11 @@ export default function ChapterLayout({
       <div className="min-h-screen">
         <AlertBanner preview={preview} loading={loading} />
         <main>{children}</main>
+        {/* <BlogHeader title={''} level={1}/> */}
+        <Header/>
+        <BlogContainer children={<div></div>}/>
+        <Sidebar children={<div></div>}/>
+        <Footer/>
       </div>
     </>
   )
