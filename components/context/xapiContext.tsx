9b151e080e8d35f19168f2ff4677e708e228dd28
JohanAOstbye <johan@ostbye.dev>
2023-02-23 14:41:31 +0800

    expanded the foundation of the xapim context

--- components/context/xapiContext.tsx
+++ components/context/xapiContext.tsx
@@ -1,5 +1,14 @@
-import { statementType } from 'lib/types/x-api'
-import React, { createContext, useContext, useMemo } from 'react'
+import { agentType } from 'lib/types/x-api/actor'
+import { authorityType } from 'lib/types/x-api/authority'
+import { statementType } from 'lib/types/x-api/statement'
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+import { useSession } from 'next-auth/react'
 
 type XapiContextType = {
   recordStatment: (statement: statementType) => void
@@ -15,10 +24,63 @@ interface SessionContextProviderProps {
 export const SessionContextProvider = ({
   children,
 }: SessionContextProviderProps) => {
+  const { data: session, status } = useSession()
+  const [agent, setAgent] = useState<agentType | undefined>(undefined)
+  const [authority, setAuthority] = useState<authorityType | undefined>(
+    undefined
+  )
+  const [relatedStatements, setRelatedStatements] = useState([])
   const recordStatment = (statement: statementType) => {
-    //TODO: implement trpc
-    console.log(statement)
+    statement.authority = authority
+
+    const response = fetch('/api/xapi/statements', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(statement),
+    })
   }
+
+  useEffect(() => {
+    if (status === 'authenticated' && session && session.user) {
+      setAgent({
+        objectType: 'Agent',
+        name: session.user.name === null ? undefined : session.user.name,
+        mbox: `mailto:${session.user.email}`,
+      })
+    }
+
+    return () => {
+      setAgent(undefined)
+      setAuthority(undefined)
+    }
+  }, [session, status])
+
+  useEffect(() => {
+    if (agent) {
+      setAuthority({
+        objectType: 'Group',
+        name: 'LMS',
+        member: [
+          agent,
+          {
+            objectType: 'Agent',
+            account: {
+              homePage: process.env.NEXTAUTH_URL
+                ? process.env.NEXTAUTH_URL
+                : 'www.protus.no',
+              name: 'protus-client',
+            },
+          },
+        ],
+      })
+      //add related statements
+    }
+    return () => {
+      setAuthority(undefined)
+    }
+  }, [agent])
 
   const memoedXapi = useMemo(
     () => ({
