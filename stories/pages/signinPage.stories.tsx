70807e927f59770c62d05a6ca7203a22ceebf4fb
Vemund Eggemoen <vemundeggemoen@vemunds-mbp.home>
2023-03-06 10:05:25 +0800

    add SessionProvider for sb

--- stories/pages/signinPage.stories.tsx
+++ stories/pages/signinPage.stories.tsx
@@ -5,6 +5,7 @@ import React, { ReactPropTypes } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { SignInPage } from 'components/pages/auth/SignInPage'
 import Layout from 'components/layout'
+import { SessionProvider } from 'next-auth/react'
 
 export default {
   /* 👇 The title prop is optional.
@@ -16,7 +17,9 @@ export default {
 } as ComponentMeta<typeof SignInPage>
 
 export const Primary: ComponentStory<typeof SignInPage> = () => (
-  <Layout>
-    <SignInPage />
-  </Layout>
+  <SessionProvider>
+    <Layout>
+      <SignInPage />
+    </Layout>
+  </SessionProvider>
 )
