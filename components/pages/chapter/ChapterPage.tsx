38d8059c67b0d82e8a18a100c3a79251df9eb600
JohanAOstbye <johan@ostbye.dev>
2023-04-07 14:51:26 +0800

    temporary disable xapi logging

--- components/pages/chapter/ChapterPage.tsx
+++ components/pages/chapter/ChapterPage.tsx
@@ -20,41 +20,41 @@ const ChapterPage = ({
   chapter: chapterType
   course: string
 }) => {
-  const { data: session, status } = useSession({ required: true })
-  const { recordStatment } = useXapi()
+  // const { data: session, status } = useSession({ required: true })
+  // const { recordStatment } = useXapi()
 
-  useEffect(() => {
-    if (status === 'authenticated' && session && session.user) {
-      recordStatment({
-        object: {
-          objectType: 'Activity',
-          id: `https://protus.no/c/${course}/${chapter.slug}`,
-          definition: {
-            name: {
-              en: chapter.title || 'Untitled chapter: ' + chapter._id,
-            },
-          },
-        },
-        actor: {
-          objectType: 'Agent',
-          name: session.user.name === null ? undefined : session.user.name,
-          mbox: `mailto:${session.user.email}`,
-        },
-        verb: {
-          id: 'http://adlnet.gov/expapi/verbs/viewed',
-          display: {
-            en: 'viewed',
-          },
-        },
-        context: {
-          platform: `${getDeviceCategory(
-            window.innerWidth,
-            window.innerHeight
-          )} ${window.navigator.userAgent}`,
-        },
-      })
-    }
-  }, [status, course])
+  // useEffect(() => {
+  //   if (status === 'authenticated' && session && session.user) {
+  //     recordStatment({
+  //       object: {
+  //         objectType: 'Activity',
+  //         id: `https://protus.no/c/${course}/${chapter.slug}`,
+  //         definition: {
+  //           name: {
+  //             en: chapter.title || 'Untitled chapter: ' + chapter._id,
+  //           },
+  //         },
+  //       },
+  //       actor: {
+  //         objectType: 'Agent',
+  //         name: session.user.name === null ? undefined : session.user.name,
+  //         mbox: `mailto:${session.user.email}`,
+  //       },
+  //       verb: {
+  //         id: 'http://adlnet.gov/expapi/verbs/viewed',
+  //         display: {
+  //           en: 'viewed',
+  //         },
+  //       },
+  //       context: {
+  //         platform: `${getDeviceCategory(
+  //           window.innerWidth,
+  //           window.innerHeight
+  //         )} ${window.navigator.userAgent}`,
+  //       },
+  //     })
+  //   }
+  // }, [status, course])
   return (
     <article>
       {preview && <AlertBanner loading={loading} />}
