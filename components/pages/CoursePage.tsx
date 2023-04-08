eb251471e449186cfa20a52e1f20135dd67dad98
JohanAOstbye <johan@ostbye.dev>
2023-04-08 11:45:55 +0800

    removed test print

--- components/pages/CoursePage.tsx
+++ components/pages/CoursePage.tsx
@@ -57,7 +57,6 @@ const CoursePage = ({
   // }, [status, course])
   return (
     <div className={style.page}>
-      <div>{JSON.stringify(course)}</div>
       <UpdateCourse course={course} />
       {course ? (
         <>
